beb64b3ab400e1eb16ec1e2e4b978ae27cdab1e4
Arsath99 <90707804+Arsath99@users.noreply.github.com>
2023-02-09 07:26:48 +0530

    part 2 completed with javafx installation

--- src/Shop.java
+++ src/Shop.java
@@ -1,6 +1,8 @@
 package src;
 
+import java.io.*;
 import java.util.*;
+import javafx.stage.*;
 
 public class Shop {
     private ArrayList<Tool> toolList;
@@ -11,6 +13,30 @@ public class Shop {
     
     public void storeTool(Tool tool) {
         toolList.add(tool);
+    }
+
+    public void readToolData() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select tool data file");
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if (selectedFile != null) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(selectedFile))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] data = line.split(",");
+                    String toolName = data[0];
+                    String itemCode = data[1];
+                    int timesBorrowed = Integer.parseInt(data[2]);
+                    boolean onLoan = Boolean.parseBoolean(data[3]);
+                    int cost = Integer.parseInt(data[4]);
+                    int weight = Integer.parseInt(data[5]);
+                    Tool tool = new Tool(toolName, itemCode, timesBorrowed, onLoan, cost, weight);
+                    toolList.add(tool);
+                }
+            } catch (IOException e) {
+                System.out.println("Error reading file: " + e.getMessage());
+            }
+        }
     }
     
     public void printAllTools() {
